d802bbfcf4ceb5b45ae90a9080e1eb3f41f73065
ChakradharA2003 <144037702+ChakradharA2003@users.noreply.github.com>
2024-03-12 23:00:14 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,27 +1,25 @@
-import {Route, Switch} from 'react-router-dom'
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
 
 import Header from './components/Header'
 import About from './components/About'
-import Home from './components/Home'
 import Contact from './components/Contact'
+import BlogsList from './components/BlogsList'
 import NotFound from './components/NotFound'
+import BlogItemDetails from './components/BlogItemDetails'
 
 import './App.css'
 
 const App = () => (
-  <div className="app-container">
-    <div className="responsive-container">
-      <Header />
-      <div className="app-body">
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/about" component={About} />
-          <Route exact path="/contact" component={Contact} />
-          <Route component={NotFound} />
-        </Switch>
-      </div>
-    </div>
-  </div>
+  <BrowserRouter>
+    <Header />
+    <Switch>
+      <Route exact path="/" component={BlogsList} />
+      <Route exact path="/blogs/:id" component={BlogItemDetails} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/contact" component={Contact} />
+      <Route component={NotFound} />
+    </Switch>
+  </BrowserRouter>
 )
 
 export default App
